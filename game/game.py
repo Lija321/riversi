94e2a934a02a2c3fc4bf99b9ffb83b40c13675d7
Lija321 <lisica.dejan@hotmail.com>
2023-05-18 03:53:12 +0200

    Game done

--- game/game.py
+++ game/game.py
@@ -47,8 +47,12 @@ class Game(object):
             print(f"{time.time() - dt:.5f}")
 
     def play_type2(self):
+        first_iter=True
         while not self.state.is_game_ended():
             print_pos_to_console(self.state)
+            if not first_iter:
+                print(f"\nAlgorithm time: {minimax_time:.3f}s")
+            else: first_iter=False
             pos=possible_moves(self.state)
             temp=[]
             for i,item in enumerate(pos):
@@ -60,22 +64,31 @@ class Game(object):
             choice=temp[int(choice)-1]
             place_piece(self.state,choice[0],choice[1])
             print_state_to_console(self.state)
+            minimax_time = time.time()
             evaluation, self.state = minimax.minimax(self.state, 6, True, self.state.player_to_move == 1, np.float16('-inf'),
                                                 np.float16('inf'))
+            minimax_time = time.time() - minimax_time
 
     def play_type3(self):
         while not self.state.is_game_ended():
+            minimax_time=time.time()
             evaluation, self.state = minimax.minimax(self.state, 6, True, self.state.player_to_move == 1, np.float16('-inf'),
                                                 np.float16('inf'))
-            print_pos_to_console(self.state)
-            pos = possible_moves(self.state)
-            temp = []
-            for i, item in enumerate(pos):
-                x, y = item
-                temp.append(item)
-                print(f"{i + 1}. {x + 1}{chr(y + 65)}")
-            choice = str(input(">> "))
-            if not (choice.isnumeric() and int(choice) - 1 in range(0, len(pos))): continue
+            minimax_time=time.time()-minimax_time
+
+            while True:
+                print_pos_to_console(self.state)
+                print(f"\nAlgorithm time: {minimax_time:.3f}s")
+                pos = possible_moves(self.state)
+                temp = []
+                for i, item in enumerate(pos):
+                    x, y = item
+                    temp.append(item)
+                    print(f"{i + 1}. {x + 1}{chr(y + 65)}")
+
+                choice = str(input(">> "))
+                if not (choice.isnumeric() and int(choice) - 1 in range(0, len(pos))): continue
+                break
             choice = temp[int(choice) - 1]
             place_piece(self.state, choice[0], choice[1])
             print_state_to_console(self.state)
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import os
+import sys
+
 from game.game import Game
 
 def main():
-    g=Game(4)
+    while True:
+        os.system("clear")
+        print("Choose mode:\n1. AI vs AI\n2. P vs AI\n3. AI v P\n4. P v P")
+        mode=str(input(">> "))
+        if not(mode.isnumeric() and int(mode) in range(0,5)):continue
+        mode=int(mode)
+        break
+    g=Game(mode)
     g.play()
 
 if __name__=="__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+    finally:
+        sys.exit(-1)
