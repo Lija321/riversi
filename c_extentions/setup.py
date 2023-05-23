7867c22ef18eaae53fc66b9778932db7d718c765
Lija321 <lisica.dejan@hotmail.com>
2023-05-23 13:19:07 +0200

    Windows compile

--- c_extentions/setup.py
+++ c_extentions/setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize("riversi_cython.pyx"),
+    ext_modules = cythonize("riversi_cython.pyx",compiler_directives={'language_level' : "3"} ),
     include_dirs = [numpy.get_include()]
 )
